728f2c54b34249b6c366ef551f4ddf09a56abbf4
alexwilliams0712 <36959865+alexwilliams0712@users.noreply.github.com>
2020-05-04 08:31:38 +0100

    Update entrypoint.zsh

--- entrypoint.zsh
+++ entrypoint.zsh
@@ -1,14 +1,13 @@
 export PATH=/usr/bin:$PATH
 
-current_file_location=$(dirname $(realpath $0))
 
 # Source shared functions and aliases
-source $current_file_location/lib/shared_profile.zsh
-source $current_file_location/lib/shared_aliases.zsh
+source ~/profile/lib/shared_profile.zsh
+source ~/profile/lib/shared_aliases.zsh
 
 # Source personal profile if it exists
 if [ -f ~/personal.zsh ]; then
     source ~/personal.zsh
 fi
 
-echo "*** Airain Profile Sourced ***"
+echo "*** Alex's Profile Sourced ***"
