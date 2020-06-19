7fa3d5a15387f4ace81a00c3a5dafc0c6c2518ab
alexwilliams0712 <alexwilliams0712@hotmail.com>
2020-06-19 19:51:22 +0100

    remove mkvirtualenv alias

--- lib/shared_aliases.zsh
+++ lib/shared_aliases.zsh
@@ -74,7 +74,6 @@ alias nah='git reset --hard;git clean -df'
 #Python Dev
 ##
 alias pylint="python -m pylint **/*.py --exit-zero"
-alias mkvirtualenv="mkvirtualenv; pip install --upgrade pip; pip install black pylint pytest"
 alias pip-compile-dev="pip-compile --no-emit-index-url --no-emit-trusted-host --output-file requirements-dev.txt requirements-dev.in"
 alias mkdevreqs="echo 'pytest\npylint\npytest-cov' > requirements-dev.in; pip-compile-dev"
 alias pytest="pytest -s -vv"
