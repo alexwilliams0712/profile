3229362f52cb2459d53ad8cd63d28667936cdce7
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2020-10-04 13:40:28 +0100

    Update entrypoint.zsh

--- entrypoint.zsh
+++ entrypoint.zsh
@@ -2,8 +2,8 @@ export PATH=/usr/bin:$PATH
 
 
 # Source shared functions and aliases
-source ~/CODE/git/profile/lib/shared_profile.zsh
-source ~/CODE/git/profile/lib/shared_aliases.zsh
+source $PROJECT_ROOT/lib/shared_profile.zsh
+source $PROJECT_ROOT/lib/shared_aliases.zsh
 
 # Source personal profile if it exists
 if [ -f ~/personal.zsh ]; then
