491d8e237a5ed128756c8ff14468cb42ad9718b7
alexwilliams0712 <alexwilliams0712@hotmail.com>
2020-11-20 22:17:43 +0000

    correct filepath

--- entrypoint.zsh
+++ entrypoint.zsh
@@ -3,8 +3,8 @@ export PATH=/usr/bin:$PATH
 PROJECT_ROOT=~/CODE/git/alexwi/profile
 
 # Source shared functions and aliases
-source $PROJECT_ROOT/lib/shared_profile.zsh
-source $PROJECT_ROOT/lib/shared_aliases.zsh
+source $PROJECT_ROOT/dotfiles/shared_profile.zsh
+source $PROJECT_ROOT/dotfiles/shared_aliases.zsh
 
 # Source personal profile if it exists
 if [ -f ~/personal.zsh ]; then
