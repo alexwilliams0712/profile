46093c15614bbf37f67e6de4c1905ae153b84c88
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2021-03-24 19:36:11 +0000

    Update setup.zsh

--- tools/setup.zsh
+++ tools/setup.zsh
@@ -1,7 +1,7 @@
 #!/bin/env zsh
 echo "setup running"
 export PATH="/usr/local/bin:$PATH"
-export PATH="/opt/local/sbin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 exit_code=0
 PROJECT_ROOT=~/CODE/git/alexwi/profile
 
