8893e0fc2adc61a314abc51621d3739b49e2148d
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2021-05-28 23:43:40 +0100

    Update shared_aliases.zsh

--- dotfiles/shared_aliases.zsh
+++ dotfiles/shared_aliases.zsh
@@ -104,3 +104,4 @@ alias dockerkillall="docker kill $'(docker ps -qa)'; docker rm $'(docker ps -qa)
 alias dockerrabbit="docker run -p 5672:5672 -p 15672:15672 -d --hostname rabbit --name rabbit rabbitmq:3.8.16-management"
 alias dockerredis="docker run -p 6379:6379 --name redis -d redis"
 alias dockermaria="docker run -p 127.0.0.1:3306:3306  --name mariadb -e MARIADB_ROOT_PASSWORD=salcap -d mariadb"
+alias dockerkubectl="kubectl --context docker-desktop"
