0fd0d2cae231e9a3519d8381c6f18a8c29ae9285
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2021-05-29 21:05:31 +0100

    Update shared_aliases.zsh

--- dotfiles/shared_aliases.zsh
+++ dotfiles/shared_aliases.zsh
@@ -101,7 +101,7 @@ alias killdeadpods="kubectl get pods --all-namespaces \
                    | xargs kubectl delete pods --force"
                    
 alias dockerkillall="docker kill $'(docker ps -qa)'; docker rm $'(docker ps -qa)'"
-alias dockerrabbit="docker run -p 5672:5672 -p 15672:15672 -d --hostname rabbit --name rabbit rabbitmq:3.8.16-management"
-alias dockerredis="docker run -p 6379:6379 --name redis -d redis"
-alias dockermaria="docker run -p 127.0.0.1:3306:3306  --name mariadb -e MARIADB_ROOT_PASSWORD=salcap -d mariadb"
+alias dockerrabbit="docker run -p 5672:5672 -p 15672:15672  --network dev --hostname rabbit --name rabbit -d rabbitmq:3.8.16-management"
+alias dockerredis="docker run -p 6379:6379 --name redis --network dev -d redis"
+alias dockermaria="docker run -p 3306:3306  --name mariadb -e MARIADB_ROOT_PASSWORD=salcap --network dev -d mariadb"
 alias dockerkubectl="kubectl --context docker-desktop"
