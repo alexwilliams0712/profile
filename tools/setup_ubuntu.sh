039bca3d06fb08630606dfa2c5492a210c8ee525
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2022-01-01 20:35:07 +0000

    Update setup_ubuntu.sh

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -82,6 +82,16 @@ create_sandbox_venv() {
     cd $HOME/profile/tools
 }
 
+installkrew() {
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+}
+
 exit_script() {
     if [[ exit_code -eq 0 ]]; then
         cd $HOME/profile
@@ -100,6 +110,7 @@ main() {
     install_apt_packages
     set_up_virtualenvwrapper
     create_sandbox_venv
+    installkrew
     exit_script
 }
 
