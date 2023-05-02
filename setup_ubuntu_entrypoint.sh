4dee51b09c52e181662780f83c174cb1c9bbd6a9
alexwilliams0712 <alexwilliams0712@hotmail.com>
2023-05-02 23:38:09 +0100

    entry

--- setup_ubuntu_entrypoint.sh
+++ setup_ubuntu_entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! command -v git >/dev/null 2>&1; then
+  echo "git is not installed, installing git."
+  sudo apt-get update
+  sudo apt-get install -y git
+fi
+
 git fetch origin && git reset --hard origin/main
 git checkout main
 git pull
