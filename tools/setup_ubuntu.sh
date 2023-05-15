b2a9001abdf938a9dc33a8413614c143acf41b78
alexwilliams0712 <alexwilliams0712@hotmail.com>
2023-05-15 15:20:56 +0100

    fix docker

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -176,7 +176,7 @@ install_chrome() {
 }
 install_and_setup_docker() {
 	sudo mkdir -m 0755 -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo \
 		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
