1c1207c61983ad2f0f1e094b65a650830870f465
alexwilliams0712 <alexwilliams0712@hotmail.com>
2023-05-17 14:50:37 +0100

    add autoremove

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -19,16 +19,6 @@ handle_error() {
 }
 trap 'handle_error $LINENO' ERR
 
-# apt_upgrader() {
-# 	sudo apt update -y
-# 	sudo apt upgrade -y
-# 	sudo apt-get update -y
-# 	sudo apt-get upgrade -y
-#     sudo apt full-upgrade -y
-#     sudo apt autoremove -y
-# }
-
-
 copy_dotfiles() {
 	mkdir -p $HOME/.config/terminator
 	cp $PROFILE_DIR/dotfiles/terminal_config $HOME/.config/terminator/config
