34e6662c407add8dc02845c180c018743ae58515
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2023-06-24 16:00:09 +0100

    Update setup_ubuntu.sh

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -188,6 +188,7 @@ install_jetbrains_toolbox() {
 }
 install_chrome() {
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ 	chmod o+r google-chrome-stable_current_amd64.deb
 	sudo apt install -y ./google-chrome-stable_current_amd64.deb
 	rm google-chrome-stable_current_amd64.deb
 }
