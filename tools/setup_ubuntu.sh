24f591b2570a73e5d0463a909ffa603568b7e595
Alex Williams <36959865+alexwilliams0712@users.noreply.github.com>
2023-06-27 09:52:39 +0100

    Update setup_ubuntu.sh

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -189,7 +189,9 @@ install_vscode() {
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-	rm -f packages.microsoft.gpg
+	apt_upgrader
+ 	sudo apt-get install -y code
+ 	rm -f packages.microsoft.gpg
 }
 install_portmaster() {
 	print_function_name
