7bd188bedd2814b2b421cba550c79da118161aa0
alexwilliams0712 <alexwilliams0712@hotmail.com>
2024-06-19 07:41:11 +0100

    print uv ver

--- tools/setup_ubuntu.sh
+++ tools/setup_ubuntu.sh
@@ -388,6 +388,7 @@ install_helm() {
 	curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 	chmod 700 get_helm.sh
 	./get_helm.sh
+	sudo rm get_helm.sh
 }
 
 install_zoom() {
